8fa4405c795090e70b2902576ecbb4b48488b482
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2018-01-15 16:19:56 +0200

    Fix core info

--- internal/api/handlers/ctx.go
+++ internal/api/handlers/ctx.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"gitlab.com/distributed_lab/logan/v3"
-	"gitlab.com/swarmfund/api/coreinfo"
 	"gitlab.com/swarmfund/api/db2/api"
 	"gitlab.com/swarmfund/api/internal/data"
 	"gitlab.com/swarmfund/api/internal/hose"
@@ -115,7 +114,7 @@ func Storage(r *http.Request) *storage.Connector {
 	return r.Context().Value(storageCtxKey).(*storage.Connector)
 }
 
-func CtxCoreInfo(s *coreinfo.Connector) func(context.Context) context.Context {
+func CtxCoreInfo(s data.CoreInfoI) func(context.Context) context.Context {
 	return func(ctx context.Context) context.Context {
 		return context.WithValue(ctx, coreInfoCtxKey, s)
 	}
