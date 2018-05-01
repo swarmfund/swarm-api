bc3d0996a1d212cba56c77290400c0494123ebb4
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2018-05-01 19:17:35 +0300

    Optional pdf

--- storage/content_type.go
+++ storage/content_type.go
@@ -2,39 +2,68 @@ package storage
 
 import (
 	"github.com/pkg/errors"
+	"gitlab.com/swarmfund/api/internal/types"
 )
 
 var (
-	allowedMediaTypes = map[string]string{
-		"image/jpeg": "jpeg",
-		"image/tiff": "tiff",
-		"image/png":  "png",
-		"image/gif":  "gif",
+	allowedMediaTypes = map[types.DocumentType]map[string]struct{}{
+		0: map[string]struct{}{
+			"image/jpeg": struct{}{},
+			"image/tiff": struct{}{},
+			"image/png":  struct{}{},
+			"image/gif":  struct{}{},
+		},
+		types.DocumentTypeFundDocument: map[string]struct{}{
+			"application/pdf": struct{}{},
+		},
+		types.DocumentTypeNavReport: map[string]struct{}{
+			"application/pdf": struct{}{},
+		},
+		types.DocumentTypeAlpha: map[string]struct{}{
+			"application/pdf": struct{}{},
+		},
+		types.DocumentTypeBravo: map[string]struct{}{
+			"application/pdf": struct{}{},
+		},
+		types.DocumentTypeCharlie: map[string]struct{}{
+			"application/pdf": struct{}{},
+		},
+		types.DocumentTypeDelta: map[string]struct{}{
+			"application/pdf": struct{}{},
+		},
+		types.DocumentTypeTokenTerms: map[string]struct{}{
+			"application/pdf": struct{}{},
+		},
+		types.DocumentTypeTokenMetrics: map[string]struct{}{
+			"application/pdf": struct{}{},
+		},
 	}
 
-	IsAllowedContentType = func(value interface{}) error {
-		str, ok := value.(string)
-		if !ok {
-			return errors.New("string expected")
+	IsAllowedContentType = func(docType types.DocumentType) func(value interface{}) error {
+		return func(value interface{}) error {
+			str, ok := value.(string)
+			if !ok {
+				return errors.New("string expected")
+			}
+			if IsContentTypeAllowed(docType, str) {
+				return nil
+			}
+			return errors.New("not allowed")
 		}
-		if IsContentTypeAllowed(str) {
-			return nil
-		}
-		return errors.New("not allowed")
 	}
 )
 
-func IsContentTypeAllowed(mediaType string) bool {
-	_, ok := allowedMediaTypes[mediaType]
-	return ok
-}
-
-// ContentTypeExtension return file extension for corresponding media type.
-// Will panic if media type is unknown, use `IsContentTypeAllowed` first
-func ContentTypeExtension(mediaType string) string {
-	ext, ok := allowedMediaTypes[mediaType]
-	if !ok {
-		panic("unknown media type")
+func IsContentTypeAllowed(docType types.DocumentType, mediaType string) bool {
+	_, ok := allowedMediaTypes[0][mediaType]
+	if ok {
+		return true
 	}
-	return ext
+	specific, ok := allowedMediaTypes[docType]
+	if ok {
+		_, ok := specific[mediaType]
+		if ok {
+			return true
+		}
+	}
+	return false
 }
