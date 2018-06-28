6d4c9441bf13ac6ded318038bc83fe58970c99a5
OleksiiKhramov <khramoff.al@gmail.com>
2018-06-28 14:11:58 +0300

    Fixed GetParticipants

--- internal/api/handlers/get_operation_participants.go
+++ internal/api/handlers/get_operation_participants.go
@@ -2,78 +2,62 @@ package handlers
 
 import (
 	"encoding/json"
+	"net/http"
+
+	. "github.com/go-ozzo/ozzo-validation"
+	"github.com/pkg/errors"
 	"gitlab.com/distributed_lab/ape"
 	"gitlab.com/distributed_lab/ape/problems"
 	"gitlab.com/swarmfund/api/db2/api"
 	"gitlab.com/swarmfund/api/internal/types"
 	"gitlab.com/swarmfund/api/resource/base"
-	"io/ioutil"
-	"net/http"
 )
 
 type (
-	ParticipantsRequest struct {
-		ForAccount   string                      `json:"for_account" valid:"required"`
+	GetParticipantsRequest struct {
+		ForAccount   string                      `json:"for_account"`
 		Participants map[int64][]api.Participant `json:"participants"`
-	}
-	Participants struct {
-		Request  ParticipantsRequest
-		Resource map[int64][]base.Participant
 	}
 )
 
+func NewGetParticipantsRequest(r *http.Request) (GetParticipantsRequest, error) {
+	var request GetParticipantsRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		return request, errors.Wrap(err, "failed to unmarshal")
+	}
+
+	return request, request.Validate()
+}
+
+func (r GetParticipantsRequest) Validate() error {
+	return ValidateStruct(&r,
+		Field(&r.ForAccount, Required),
+		Field(&r.Participants, Required),
+	)
+}
+
 func GetParticipants(w http.ResponseWriter, r *http.Request) {
 
-	body, err := ioutil.ReadAll(r.Body)
+	request, err := NewGetParticipantsRequest(r)
+
 	if err != nil {
-		Log(r).WithError(err).Error("Incorrect body")
 		ape.RenderErr(w, problems.BadRequest(err)...)
 	}
 
-	var res Participants
+	var accountIDs []string
 
-	err = json.Unmarshal(body, &res.Request)
-	if err != nil {
-		Log(r).WithError(err).Error("Can't unmarshal request")
-		ape.RenderErr(w, problems.BadRequest(err)...)
+	for _, op := range request.Participants {
+		for pi := range op {
+			accountIDs = append(accountIDs, string(op[pi].AccountID))
+		}
 	}
-
-	accountIDs := res.Request.fetchIDs()
 
 	users, err := UsersQ(r).ByAddresses(accountIDs)
 	if err != nil {
 		Log(r).WithError(err).Error("Can't find users")
 		ape.RenderErr(w, problems.InternalError())
 	}
-	res.Request = res.Request.update(users)
-
-	res.Resource = res.Request.populate()
-
-	json.NewEncoder(w).Encode(&res.Resource)
-}
-
-func (request ParticipantsRequest) populate() (response map[int64][]base.Participant) {
-	for op, participants := range request.Participants {
-		for _, participant := range participants {
-			var r base.Participant
-			r.Populate(&participant)
-			response[op] = append(response[op], r)
-		}
-	}
-
-	return
-}
-
-func (request ParticipantsRequest) fetchIDs() (accountIDs []string) {
-	for _, op := range request.Participants {
-		for pi := range op {
-			accountIDs = append(accountIDs, string(op[pi].AccountID))
-		}
-	}
-	return
-}
-
-func (request ParticipantsRequest) update(users []api.User) ParticipantsRequest {
 
 	usersMap := map[types.Address]api.User{}
 	for _, user := range users {
@@ -89,5 +73,15 @@ func (request ParticipantsRequest) update(users []api.User) ParticipantsRequest 
 			}
 		}
 	}
-	return request
+
+	response := make(map[int64][]base.Participant)
+	for op, participants := range request.Participants {
+		for _, participant := range participants {
+			var r base.Participant
+			r.Populate(&participant)
+			response[op] = append(response[op], r)
+		}
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
