610829c10bfafda572ba94b9b081b4ba844b8861
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2018-08-06 18:31:38 +0300

    Random salt

--- internal/lorem/token.go
+++ internal/lorem/token.go
@@ -1,6 +1,12 @@
 package lorem
 
-import "math/rand"
+import (
+	"math/rand"
+
+	"encoding/base64"
+
+	"github.com/pkg/errors"
+)
 
 var (
 	letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -13,6 +19,15 @@ func randomString(n int, source string) string {
 		b[i] = source[rand.Intn(len(source))]
 	}
 	return string(b)
+}
+
+func Salt() string {
+	r := make([]byte, 30)
+	_, err := rand.Read(r)
+	if err != nil {
+		panic(errors.Wrap(err, "failed to read random"))
+	}
+	return base64.StdEncoding.EncodeToString(r)
 }
 
 func Token() string {
