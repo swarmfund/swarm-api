87178521b984664bb1298c9d3ec2686cea5afbcd
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2018-08-11 15:57:57 +0300

    Fix info

--- config/kyc_index.go
+++ config/kyc_index.go
@@ -3,6 +3,6 @@ package config
 import "net/url"
 
 func (v *ViperConfig) KYCIndex() *url.URL {
-	u, _ := url.Parse("http://localhost:7006")
+	u, _ := url.Parse("http://kycprovider:7006")
 	return u
 }
