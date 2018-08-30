67d41f5a121a6f6039b8a3d3be7a644eaaca9268
Skriabin <artemkaskriabin@gmail.com>
2018-08-30 10:52:23 +0300

    Fix test

--- config/geoinfo_test.go
+++ config/geoinfo_test.go
@@ -7,35 +7,40 @@ import (
 )
 
 func TestViperConfig_GeoInfo(t *testing.T) {
-	raw := &mockRawGetter{}
-	raw.On("GetStringMap", "geo_info").Return(map[string]interface{}{
-		"api_url":    "http://api.ipstack.com",
-		"access_key": "fb170f98697192973f33434cb35157b4",
-	}).Once()
-	defer raw.AssertExpectations(t)
+	t.Run("success", func(t *testing.T) {
+		raw := &mockRawGetter{}
+		raw.On("GetStringMap", "geo_info").Return(map[string]interface{}{
+			"api_url":    "http://api.ipstack.com",
+			"access_key": "fb170f98697192973f33434cb35157b4",
+		}).Once()
+		defer raw.AssertExpectations(t)
 
-	config := newViperConfig(raw)
+		config := newViperConfig(raw)
 
-	assert.NotNil(t, config.GeoInfo())
-}
+		assert.NotNil(t, config.GeoInfo())
+	})
 
-func TestViperConfig_GeoInfoPanics(t *testing.T) {
-	raw := &mockRawGetter{}
-	raw.On("GetStringMap", "geo_info").Return(map[string]interface{}{
-		"api_url": "http://api.ipstack.com",
-	}).Once()
-	defer raw.AssertExpectations(t)
+	t.Run("panic because access key not specified", func(t *testing.T) {
+		raw := &mockRawGetter{}
+		raw.On("GetStringMap", "geo_info").Return(map[string]interface{}{
+			"api_url": "http://api.ipstack.com",
+		}).Once()
+		defer raw.AssertExpectations(t)
 
-	config := newViperConfig(raw)
+		config := newViperConfig(raw)
 
-	assert.Panics(t, func() { config.GeoInfo() })
+		assert.Panics(t, func() { config.GeoInfo() })
+	})
 
-	raw = &mockRawGetter{}
-	raw.On("GetStringMap", "geo_info").Return(map[string]interface{}{
-		"access_key": "fb170f98697192973f33434cb35157b4",
-	}).Once()
+	t.Run("panic because api url not specified", func(t *testing.T) {
+		raw := &mockRawGetter{}
+		raw.On("GetStringMap", "geo_info").Return(map[string]interface{}{
+			"access_key": "fb170f98697192973f33434cb35157b4",
+		}).Once()
+		defer raw.AssertExpectations(t)
 
-	config = newViperConfig(raw)
+		config := newViperConfig(raw)
 
-	assert.Panics(t, func() { config.GeoInfo() })
+		assert.Panics(t, func() { config.GeoInfo() })
+	})
 }
